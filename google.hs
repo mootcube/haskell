9860d8209d0d7c5223579415c6d443cd5673c568
MoOT <mathieu.chataigner@gmail.com>
2012-04-16 16:07:04 +0200

    translate complete

--- google.hs
+++ google.hs
@@ -10,10 +10,10 @@ test a=
 
 alpha = sort(nub (zip "y qeeejp mysljylc kd kxveddknmc re jsicpdrysirbcpc ypc rtcsra dkh wyfrepkym veddknkmkrkcdde kr kd eoya kw aej tysr re ujdr lkgc jv" "a zooour language is impossible to understandthere are twenty six factorial possibilitiesso it is okay if you want to just give up" ))
 
-myGetChar x ((a,b):ls) = case a of
-  x -> b
-  _ -> myGetChar x ls
-myGetChar _ [] = ' '
+myGetChar _ [] = '\0'
+myGetChar x ((a,b):ls) 
+  |x==a = b
+  |x/=a = myGetChar x ls
 
 translate xs = map translateChar xs
-               where translateChar x=myGetChar x alpha
+               where translateChar x = myGetChar x alpha
