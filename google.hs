41f743685c3f7fcb0f3a6949d0e10c31ffcdcba3
MoOT <mathieu.chataigner@gmail.com>
2012-04-17 14:29:42 +0200

    start io

--- google.hs
+++ google.hs
@@ -1,5 +1,5 @@
 import Data.List
-
+import System.IO
 
 
 test a=
@@ -10,10 +10,23 @@ test a=
 
 alpha = sort(nub (zip "zy qeeejp mysljylc kd kxveddknmc re jsicpdrysirbcpc ypc rtcsra dkh wyfrepkym veddknkmkrkcdde kr kd eoya kw aej tysr re ujdr lkgc jv" "qa zooour language is impossible to understandthere are twenty six factorial possibilitiesso it is okay if you want to just give up" ))
 
-myGetChar _ [] = '\0'
+myGetChar x [] = x
 myGetChar x ((a,b):ls) 
   |x==a = b
   |x/=a = myGetChar x ls
 
 translate xs = map translateChar xs
                where translateChar x = myGetChar x alpha
+
+main::IO ()
+main=interact translate
+  
+titi::IO ()  
+titi=
+  do
+  file<-openFile "input.txt" ReadMode
+  n<-hGetLine file
+  a<-hGetContents file
+  let b=translate a
+  putStrLn b
+  hClose file
